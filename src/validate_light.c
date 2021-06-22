cba7f2d17bd6592fb8e8d64f1349bb709c5c81bb
mde-figu <markusfwirz@gmail.com>
2021-06-22 00:51:28 -0200

    a wild mergesort appears, its super shitty

--- src/validate_light.c
+++ src/validate_light.c
@@ -6,7 +6,7 @@
 /*   By: mde-figu <mde-figu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/05 16:20:30 by mde-figu          #+#    #+#             */
-/*   Updated: 2021/06/18 22:43:10 by mde-figu         ###   ########.fr       */
+/*   Updated: 2021/06/22 00:41:40 by mde-figu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,14 @@
 int	validate_light(char **entry_t)
 {
 	if (str_ptr_len(entry_t) != 4)
-		return (error_list(29));
-	if (!check_coordinate(entry_t[1]))
 		return (error_list(30));
+	if (!check_coordinate(entry_t[1]))
+		return (error_list(31));
 	if (!is_num(entry_t[2]))
-		return (error_list(31));
-	if (ft_atof(entry_t[2]) < 0 || ft_atof(entry_t[2]) > 1)
-		return (error_list(31));
-	if (!check_color(entry_t[3]))
 		return (error_list(32));
+	if (ft_atof(entry_t[2]) < 0 || ft_atof(entry_t[2]) > 1)
+		return (error_list(32));
+	if (!check_color(entry_t[3]))
+		return (error_list(33));
 	return (1);
 }
